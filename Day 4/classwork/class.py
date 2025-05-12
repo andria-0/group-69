921015bb3821955a422eed55bcd37fb9c03118f3
andria-0 <andriasologashvili@gmail.com>
2025-05-12 15:20:10 +0400

    first commit

---
+++ Day 4/classwork/class.py
@@ -0,0 +1,11 @@
+#1)შექმენით ცვლადი სადაც მომხარებელს შემოატანინებ თავის ასაკს, input ის საშუალებით, და მერე შეამოწმე მეტია ის თუ არა 18 ზე, გამოიყენეტ შედარების ოპერატორები
+
+age = input("რამდენი წლის ხარ შენ? : ")
+
+print (age >= str(18))
+
+#2)რას გამოიტანს ეს კოდი(ახენით დეტალურად კომენტარებით) print(True and False or 5>2 and False or False and True)
+#true and false = false
+#5>2 and false = false
+#false and true = false
+#false or false or false = false
