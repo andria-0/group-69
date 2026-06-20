18f4edeb9a5c735f57a61c28ba17c2c9f7ad6b7d
andria-0 <andriasologashvili@gmail.com>
2026-06-20 19:50:34 +0400

    first commit

---
+++ ტესტები/jojo part 4/python/test.py
@@ -0,0 +1,20 @@
+# for i in range(102):
+#     if i%2 == 0:
+#         print(i)
+
+
+# for i in range(20,121):
+#     if i%2 == 1:
+#         print(i)
+
+def grade_sistem(grd):
+    if grd>=90:
+        print("A")
+    elif 90>grd>=80:
+        print("B")
+    elif 80>grd>=70:
+        print("C")
+    else :
+        print("F")
+
+grade_sistem(20)
